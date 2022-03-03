94abcabbef78569a0b0effc2e94d53d2b19607f1
ARIFULHAQUE1 <arifulhaquearif2326@gmail.com>
2022-03-03 17:58:22 +0600

    third commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const API_KEY = `76b4d4a61ff5db682a0e19112a6c4aa1`;
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
 
     fetch(url)
         .then(response => response.json())
@@ -21,7 +21,7 @@ const displayTemperature = (tempeture) => {
     // city cloud
     setInnerText('cloud', tempeture.weather[0].main)
     // set icon image
-    const url = `http://openweathermap.org/img/wn/${tempeture.weather[0].icon}@2x.png`
+    const url = `https://openweathermap.org/img/wn/${tempeture.weather[0].icon}@2x.png`
     const weatherIcon = document.getElementById('weather-icon')
     weatherIcon.setAttribute('src', url)
     console.log(tempeture)
